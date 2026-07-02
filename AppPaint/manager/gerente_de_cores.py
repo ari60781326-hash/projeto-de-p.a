1163fd9b5002901a33458a1c0a89dedc8ad70d82
Raí.Santos <70606883+F3ryG4rd@users.noreply.github.com>
2026-07-01 22:26:56 -0300

    Update gerente_de_cores.py

--- AppPaint/manager/gerente_de_cores.py
+++ AppPaint/manager/gerente_de_cores.py
@@ -1,23 +1,23 @@
 class GerenteDeCores:   
     ''' 
-    define as cores das figuras
-'''
+    Define as cores das figuras
+    '''
     def __init__ (self):
         self.cor_linha= 'black' #cor padrao, pra que seria rosa?
         self.cor_de_preenchimento=''
         
         
-    def color_select_outline(self, novaCor): #cor que o usuario escolheu
-        self.cor_linha= novaCor
+    def color_select_outline(self, nova_cor): #cor que o usuario escolheu
+        self.cor_linha= nova_cor
 
-    def color_select_fill(self, novaCor): #cor que o usuario escolheu
-        self.cor_de_preenchimento= novaCor
+    def color_select_fill(self, nova_cor): #cor que o usuario escolheu
+        self.cor_de_preenchimento= nova_cor
         
     def wtfIsThisColor (self):
         return self.cor_linha #a cor que vai usar nas figuras
     
     def cor_atual_da_linha (self):
-        return self.wtfIsThisColor()
+        return self.wtf_is_this_color()
 
     def cor_atual_do_preenchimento(self):
         return self.cor_linha
